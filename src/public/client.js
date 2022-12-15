95d140e8c62e543b29a415167c843b78ce02043a
Youssef Girgeis <youssef_girgeis@hotmail.com>
2022-12-14 21:13:18 -0500

    build the initial UI

--- src/public/client.js
+++ src/public/client.js
@@ -1,8 +1,13 @@
-let store = {
-  user: { name: 'Youssef' },
-  apod: '',
-  rovers: ['Curiosity', 'Opportunity', 'Spirit'],
-};
+const store = Immutable.Map({
+  //user: { name: 'Youssef' },
+  //apod: 'test',
+  rovers: Immutable.List(['Curiosity', 'Opportunity', 'Spirit']),
+});
+
+// const store2 = store.set('user', { name: 'john' });
+// console.log(store.get('user'));
+// console.log(store2.get('user'));
+// console.log(store.get('rovers').toJS()[0]);
 
 // add our markup to the page
 const root = document.getElementById('root');
@@ -20,84 +25,112 @@ const render = async (root, state) => {
 const App = (state) => {
   let { rovers, apod } = state;
 
+  //   return `
+  //         <header></header>
+  //         <main>
+  //             ${Greeting(store.user.name)}
+  //             <section>
+  //                 <h3>Put things on the page!</h3>
+  //                 <p>Here is an example section.</p>
+  //                 <p>
+  //                     One of the most popular websites at NASA is the Astronomy Picture of the Day. In fact, this website is one of
+  //                     the most popular websites across all federal agencies. It has the popular appeal of a Justin Bieber video.
+  //                     This endpoint structures the APOD imagery and associated metadata so that it can be repurposed for other
+  //                     applications. In addition, if the concept_tags parameter is set to True, then keywords derived from the image
+  //                     explanation are returned. These keywords could be used as auto-generated hashtags for twitter or instagram feeds;
+  //                     but generally help with discoverability of relevant imagery.
+  //                 </p>
+  //                 ${ImageOfTheDay(apod)}
+  //             </section>
+  //         </main>
+  //         <footer></footer>
+  //     `;
   return `
-        <header></header>
-        <main>
-            ${Greeting(store.user.name)}
-            <section>
-                <h3>Put things on the page!</h3>
-                <p>Here is an example section.</p>
-                <p>
-                    One of the most popular websites at NASA is the Astronomy Picture of the Day. In fact, this website is one of
-                    the most popular websites across all federal agencies. It has the popular appeal of a Justin Bieber video.
-                    This endpoint structures the APOD imagery and associated metadata so that it can be repurposed for other
-                    applications. In addition, if the concept_tags parameter is set to True, then keywords derived from the image
-                    explanation are returned. These keywords could be used as auto-generated hashtags for twitter or instagram feeds;
-                    but generally help with discoverability of relevant imagery.
-                </p>
-                ${ImageOfTheDay(apod)}
-            </section>
-        </main>
-        <footer></footer>
-    `;
+    <div class="btns-container">
+    ${renderRoversBtns(state)}
+    </div>
+  `;
+};
+
+const renderRoversBtns = (state) => {
+  const rovers = state.get('rovers').toJS();
+  return rovers
+    .map((rover) => {
+      return `<button class="rover-btn">${rover}</button>`;
+    })
+    .join('');
 };
 
 // listening for load event because page should load before any JS is called
 window.addEventListener('load', () => {
   render(root, store);
+  //   document
+  //     .querySelector('.rover')
+  //     .addEventListener('click', (e) => console.log(e.target.textContent));
 });
 
 // ------------------------------------------------------  COMPONENTS
 
 // Pure function that renders conditional information -- THIS IS JUST AN EXAMPLE, you can delete it.
-const Greeting = (name) => {
-  if (name) {
-    return `
-            <h1>Welcome, ${name}!</h1>
-        `;
-  }
+// const Greeting = (name) => {
+//   if (name) {
+//     return `
+//             <h1>Welcome, ${name}!</h1>
+//         `;
+//   }
 
-  return `
-        <h1>Hello!</h1>
-    `;
-};
+//   return `
+//         <h1>Hello!</h1>
+//     `;
+// };
 
 // Example of a pure function that renders infomation requested from the backend
-const ImageOfTheDay = (apod) => {
-  // If image does not already exist, or it is not from today -- request it again
-  const today = new Date();
-  const photodate = new Date(apod.date);
-  console.log(photodate.getDate(), today.getDate());
+// const ImageOfTheDay = (apod) => {
+//   // If image does not already exist, or it is not from today -- request it again
+//   const today = new Date();
+//   const photodate = new Date(apod.date);
+//   console.log(photodate.getDate(), today.getDate());
 
-  console.log(photodate.getDate() === today.getDate());
-  if (!apod || apod.date === today.getDate()) {
-    getImageOfTheDay(store);
-  }
+//   console.log(photodate.getDate() === today.getDate());
+//   if (!apod || apod.date === today.getDate()) {
+//     getImageOfTheDay(store);
+//   }
 
-  // check if the photo of the day is actually type video!
-  if (apod.media_type === 'video') {
-    return `
-            <p>See today's featured video <a href="${apod.url}">here</a></p>
-            <p>${apod.title}</p>
-            <p>${apod.explanation}</p>
-        `;
-  } else {
-    return `
-            <img src="${apod.image.url}" height="350px" width="100%" />
-            <p>${apod.image.explanation}</p>
-        `;
-  }
-};
+//   // check if the photo of the day is actually type video!
+//   if (apod.media_type === 'video') {
+//     return `
+//             <p>See today's featured video <a href="${apod.url}">here</a></p>
+//             <p>${apod.title}</p>
+//             <p>${apod.explanation}</p>
+//         `;
+//   } else {
+//     return `
+//             <img src="${apod.image.url}" height="350px" width="100%" />
+//             <p>${apod.image.explanation}</p>
+//         `;
+//   }
+// };
 
 // ------------------------------------------------------  API CALLS
 
 // Example API call
-const getImageOfTheDay = (state) => {
-  let { apod } = state;
+// const getImageOfTheDay = (state) => {
+//   let { apod } = state;
 
-  fetch(`http://localhost:3000/apod`)
-    .then((res) => res.json())
-    .then((apod) => updateStore(store, { apod }));
+//   fetch(`http://localhost:3000/apod`)
+//     .then((res) => res.json())
+//     .then((apod) => updateStore(store, { apod }));
 
-  return data;
+//   return data;
+// };
+
+const getRoverData = async (state) => {
+  const response = await fetch(`http://localhost:3000/rover`);
+
+  try {
+    const roverData = await response.json();
+    return roverData;
+  } catch (error) {
+    console.log(error);
+  }
 };
